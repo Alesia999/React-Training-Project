0c397f959da7a9626f1e22b6303cdc556b7a6ddb
Alesia999 <73301282+Alesia999@users.noreply.github.com>
2022-05-09 11:37:46 +0200

    add forms

---
+++ social-page/src/app/utils/validator.js
@@ -0,0 +1,50 @@
+export function validator(data, config) {
+  const errors = {};
+  function validate(validateMethod, data, config) {
+    let statusValid;
+
+    switch (validateMethod) {
+      case "isRequired":
+        statusValid = data.trim() !== "";
+        break;
+
+      case "isEmail":
+        const emailRegExp = /^\S+@\S+\.\S+$/g;
+
+        statusValid = emailRegExp.test(data);
+        break;
+
+      case "isCapitalSymbol":
+        const capitalRegExp = /[A-Z]+/g;
+
+        statusValid = capitalRegExp.test(data);
+        break;
+
+      case "containsDigit":
+        const digitRegExp = /\d+/g;
+
+        statusValid = digitRegExp.test(data);
+        break;
+      case "min":
+        statusValid = data.length >= config.value;
+        break;
+
+      default:
+        break;
+    }
+    if (!statusValid) return config.message;
+  }
+  for (const fieldName in data) {
+    for (const validateMethod in config[fieldName]) {
+      const error = validate(
+        validateMethod,
+        data[fieldName],
+        config[fieldName][validateMethod]
+      );
+      if (error && !errors[fieldName]) {
+        errors[fieldName] = error;
+      }
+    }
+  }
+  return errors;
+}
